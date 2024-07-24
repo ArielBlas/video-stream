7bc50907a52184b48d705069c83d98c748a184db
Ariel Blas <arielwalconblas@gmail.com>
2024-07-24 16:53:41 -0300

    change: api index

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
-const app = require("../src/index");
+const app = require("../src/app");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
+});
 
 module.exports = app;
